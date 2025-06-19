a251b3c2b9db4e1d0b2a7ef410ad54eb4eb58d60
Pushpendra Kushwaha <pk1592098@gmail.com>
2025-06-19 14:28:51 +0530

    main class name corrected

--- src/main/java/com/infra_form/InfraFormApplication.java
+++ src/main/java/com/infra_form/InfraFormApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InfraFormServiceApplication {
+public class InfraFormApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InfraFormServiceApplication.class, args);
+		SpringApplication.run(InfraFormApplication.class, args);
 	}
 
 }
